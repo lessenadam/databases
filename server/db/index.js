b5438aa45a33e3c6859a74294089a0171e3197b0
lessenadam <lessena@gmail.com>
2016-05-13 17:12:05 -0700

    Starting

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,9 @@
 var mysql = require('mysql');
 
+// mysql -u root -p
+
+// USE chat
+
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
